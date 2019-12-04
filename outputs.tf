6161f28304988d84e2187dcf0ad3d391ed3c3487
Grant Orchard <go@hashicorp.com>
2019-12-04 17:15:05 +1100

    Added s3 output and corrected VPC output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
-    value = "${module.vpc.aws_vpc.this.id}"
+    value = "${module.vpc.aws_vpc.outputs.vpc_id}"
+}
+
+output "s3_id" {
+    value = "${module.s3_bucket.outputs.s3_bucket_id}"
 }
